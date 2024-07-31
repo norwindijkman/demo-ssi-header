586ce0e80d92ce67decff62b69ad2d4b8f27682d
Norwin Dijkman <norwin.dijkman@plathena.com>
2024-07-31 14:26:21 +0200

    add demo header

---
+++ pages/header/index.tsx
@@ -0,0 +1,11 @@
+import HelloWorld from '../../src/components/HelloWorld';
+
+const ComponentPage = () => {
+  return (
+    <div>
+      <HelloWorld />
+    </div>
+  );
+};
+
+export default ComponentPage;
